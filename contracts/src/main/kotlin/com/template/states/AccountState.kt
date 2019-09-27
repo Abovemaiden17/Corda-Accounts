7629dadae9ebc3610da58c23c3bb55ad4975c053
Arnulfo Quijoy <arnulfo.quijoy@mcveanpacific.com>
2019-09-27 10:20:19 +0800

    Modified code for account

---
+++ contracts/src/main/kotlin/com/template/states/AccountState.kt
@@ -0,0 +1,19 @@
+package com.template.states
+
+import com.template.AccountContract
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
+import java.security.PublicKey
+
+@BelongsToContract(AccountContract::class)
+data class AccountState(val value: Int,
+                           val lender: PublicKey,
+                           val borrower: PublicKey,
+                           override val linearId: UniqueIdentifier = UniqueIdentifier())
+    : LinearState {
+    //The public keys of the involved parties.
+    override val participants: List<AbstractParty> get() = listOf(lender, borrower).map { AnonymousParty(it) }
+}
